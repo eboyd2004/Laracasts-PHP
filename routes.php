5d70e13fae3978759f5c493747ca6c9695674dad
gn-eboyd <ethan.boyd@nbcuni.com>
2024-08-27 16:18:09 +0100

    episode 33 end

--- routes.php
+++ routes.php
@@ -1,10 +1,9 @@
 <?php
 
-return [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes/index.php',
-    '/note' => 'controllers/notes/show.php',
-    '/notes/create' => 'controllers/notes/create.php',
-    '/contact' => 'controllers/contact.php',
-];
+$router->get('/', 'controllers/index.php');
+$router->get('/about', 'controllers/about.php');
+$router->get('/contact', 'controllers/contact.php');
+
+$router->get('/notes', 'controllers/notes/index.php');
+$router->delete('/note', 'controllers/notes/show.php');
+$router->post('/note/create', 'controllers/notes/create.php');
