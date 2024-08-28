91f9afbc8914f88cb23ae7a5f0cbace21ef649a4
gn-eboyd <ethan.boyd@nbcuni.com>
2024-08-28 16:59:23 +0100

    episode 41 end

--- Core/Router.php
+++ Core/Router.php
@@ -1,58 +1,77 @@
 <?php
 
 namespace Core;
-class Router {
-    protected $routes = [];
 
-    public function add($method, $uri, $controller){
-        $this->routes[] = [
-            'uri' => $uri,
-            'controller' => $controller,
-            'method' => $method
-        ];
-    }
-    public function get($uri, $controller)
-    {
-        $this->add('GET', $uri, $controller);
-    }
+use Core\Middleware\Auth;
+use Core\Middleware\Guest;
+use Core\Middleware\Middleware;
 
-    public function post($uri, $controller)
-    {
-        $this->add('POST', $uri, $controller);
-    }
+class Router
+{
+	protected $routes = [];
 
-    public function delete($uri, $controller)
-    {
-        $this->add('DELETE', $uri, $controller);
-    }
+	public function add($method, $uri, $controller)
+	{
+		$this->routes[] = [
+			'uri' => $uri,
+			'controller' => $controller,
+			'method' => $method,
+			'middleware' => null,
+		];
 
-    public function patch($uri, $controller)
-    {
-        $this->add('PATCH', $uri, $controller);
-    }
+		return $this;
+	}
 
-    public function put($uri, $controller)
-    {
-        $this->add('PUT', $uri, $controller);
-    }
+	public function get($uri, $controller)
+	{
+		return $this->add('GET', $uri, $controller);
+	}
 
-    public function route($uri, $method)
-    {
-        foreach ($this->routes as $route){
-            if($route['uri'] === $uri && $route['method'] === strtoupper($method)){
-                return require base_path($route['controller']);
-            }
-        }
+	public function post($uri, $controller)
+	{
+		return $this->add('POST', $uri, $controller);
+	}
 
-        $this->abort();
-    }
+	public function delete($uri, $controller)
+	{
+		return $this->add('DELETE', $uri, $controller);
+	}
 
-    protected function abort($code = 404)
-    {
-        http_response_code($code);
+	public function patch($uri, $controller)
+	{
+		return $this->add('PATCH', $uri, $controller);
+	}
 
-        require base_path("views/{$code}.php");
+	public function put($uri, $controller)
+	{
+		return $this->add('PUT', $uri, $controller);
+	}
 
-        die();
-    }
+	public function only($key)
+	{
+		$this->routes[array_key_last($this->routes)]['middleware'] = $key;
+
+		return $this;
+	}
+
+	public function route($uri, $method)
+	{
+		foreach ($this->routes as $route) {
+			if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+				Middleware::resolve($route['middleware']);
+
+				return require base_path($route['controller']);
+			}
+		}
+		$this->abort();
+	}
+
+	protected function abort($code = 404)
+	{
+		http_response_code($code);
+
+		require base_path("views/{$code}.php");
+
+		die();
+	}
 }
--- controllers/index.php
+++ controllers/index.php
@@ -1,8 +1,5 @@
 <?php
 
-$_SESSION['name'] = 'Ethan';
-
-
 view("index.view.php", [
     'heading' => 'Home',
 ]);
